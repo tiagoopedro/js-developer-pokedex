887a312cb832563b878e2dc54a3c61ebf0227a17
tiagoopedro <c.tiagopedro@hotmail.com>
2024-11-18 13:11:15 -0300

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,7 +83,7 @@ const moreDetails = (pokemon) =>{
         pokemonList.innerHTML = details + pokemonList.innerHTML
 }
 
-const pegalogo = async (id) => {
+const detailButton = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
     const pokemon = await res.json()
