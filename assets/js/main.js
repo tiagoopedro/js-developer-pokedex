8be1f8fb0c56c9d5a4af0eb2446d2a58131aa7cf
tiagoopedro <c.tiagopedro@hotmail.com>
2024-11-18 13:09:53 -0300

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,53 +20,19 @@ function convertPokemonToLi(pokemon) {
 
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
-            </div>  <button id="detailButton" onClick="detailButton()" type="button">
-                Details
-            </button>
+            </div>  
         </li>
         `
          
 }
 
-function moreDetails(pokemon) {
-    const types = pokemon.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
 
-    pokemon.types = types
-    pokemon.type = type
-
-    const photo = pokemon.sprites.other.dream_world.front_default
-     const details = `
-    <div id="popup" class="animate__animated animate__fadeIn">
-        <div id="detailPokemon">
-            <button id="closeBtn" onClick="close()">Close</button>
-
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span> 
-            <span class="profile">${pokemon.weight/10} Kg</span>
-            <span class="profile">${pokemon.height}0 cm</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.abilities.map((ability) => `<li class="hab ${ability}">${ability}</li>`).join('')}
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div> </div></div>
-        </li>`
-        pokemonList.innerHTML = details + pokemonList.innerHTML
-}
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
        /* const detailsList = pokemons.map(moreDetails).join('')*/
-        pokemonList.innerHTML += newHtml
-       
-        
-     
+        pokemonList.innerHTML += newHtml          
     }); 
 }
 
@@ -85,13 +51,43 @@ loadMoreButton.addEventListener('click', () => {
     }
 })
 
+const moreDetails = (pokemon) =>{
+    const types = pokemon.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
 
-const detailButton = (id) => {
+    pokemon.types = types
+    pokemon.type = type
+
+    const photo = pokemon.sprites.other.dream_world.front_default
+    const details = `
+     
+    <div id="popup" class="animate__animated animate__fadeIn">
+        <div id="detailPokemon">
+            <button id="closeBtn" onClick="close()">Close</button>
+
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span> 
+            <span class="profile">${pokemon.weight/10} Kg</span>
+            <span class="profile">${pokemon.height}0 cm</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.abilities.map((ability) => `<li class="hab ${ability}">${ability}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${photo}"
+                     alt="${pokemon.name}">
+            </div> </div></div>
+        </li>`
+        pokemonList.innerHTML = details + pokemonList.innerHTML
+}
+
+const pegalogo = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-    return fetch(url)
-    .then((pokemon) => pokemon.json)
-    .then((detailRequests) => Promise.all(detailRequests))
-    .then(moreDetails(pokemons))
+    const res = await fetch(url)
+    const pokemon = await res.json()
+    moreDetails(pokemon)
 }
 
 const close = () =>{
